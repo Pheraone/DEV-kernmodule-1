5980d70946e20b4f0ea54cc38f5ff0dcb3593dc7
Selawen <37039183+Selawen@users.noreply.github.com>
2020-09-12 14:52:44 +0200

    Instantiate Grid
    - now instantiates a grid with randomly distributed walkable and non-walkable tiles
    - deleted temporary cell and levelgenerator with monobehaviours

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    LevelGeneration _levelGeneration;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _levelGeneration = new LevelGeneration();
     }
 
     // Update is called once per frame
