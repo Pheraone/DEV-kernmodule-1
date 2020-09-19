88f9ff360e31dec89b7bb423f7b3571849ce33a2
Smos Bois <simondeb1505@gmail.com>
2020-09-19 16:27:55 +0200

    Input Handler
    Player now moves like pac-man. Input can be edited at any time. (gotta add code for unbind first).

---
+++ Assets/Scripts/Commands/LeftCommand.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftCommand : ICommand
+{
+    public Vector3 Execute(GameObject actor)
+    {
+        //player goes left
+
+        return new Vector2(-1,0);
+    }
+}
