94fc99838738e0729e74dc797222033a22ef0f4f
Pheraone <brechje@me.com>
2020-09-20 20:37:54 +0200

    Merge development into AI_FSM
    Merge develelopment into AI_FSM
    +New script with pseudo

---
+++ Assets/Scripts/Enemy/Pathfinder.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Pathfinder
+{
+    //Openlist
+    //ClosedList
+
+    //LOOP:
+    //currentnode = Node in open with the lowest FCost
+    //remove current from open
+    //add current to closed
+
+    //if(currentNode == targetnode)
+    //{
+        //return
+    //}
+
+    //forech(neighbour of the currentNode)
+    //{
+        //if(neighbour is not taversable || neighbour is in closedList)
+            //{
+                //skip to next neighbour
+            //}
+
+        //if(new path to neighbour is shorter || neighbour is not in openList)
+            //{
+                //set fCost of neighbour
+                //set parent of neighbour to current
+            //}
+        //if(neighbour is not in openList)
+            //{
+                //add neigbour to open
+            //}
+    //}
+
+}
